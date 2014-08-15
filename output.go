53501c430c314f3c1bc6359c6a2ec05ac148003e
Chance Zibolski <chance.zibolski@gmail.com>
2014-08-15 14:34:17 -0700

    Initial commit. Working nats output.

---
+++ output.go
@@ -0,0 +1,107 @@
+/***** BEGIN LICENSE BLOCK *****
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# The Initial Developer of the Original Code is the Mozilla Foundation.
+# Portions created by the Initial Developer are Copyright (C) 2014
+# the Initial Developer. All Rights Reserved.
+#
+# Contributor(s):
+#   Chance Zibolski (chance.zibolski@gmail.com)
+#
+# ***** END LICENSE BLOCK *****/
+
+package nats
+
+import (
+	"errors"
+	"github.com/mozilla-services/heka/pipeline"
+	"time"
+
+	"github.com/apcera/nats"
+)
+
+type NatsOutputConfig struct {
+	Url            string   `toml:"url"`
+	Servers        []string `toml:"servers"`
+	NoRandomize    bool     `toml:"no_randomize"`
+	Subject        string   `toml:"subject"`
+	AllowReconnect bool     `toml:"reconnect"`
+	MaxReconnect   int      `toml:"max_reconnects"`
+	// Time in Seconds to wait before reconnecting
+	ReconnectWait uint32 `toml:"reconnect_wait"`
+	// Timeout in Milliseconds
+	Timeout uint32 `toml:"timeout"`
+}
+
+type NatsOutput struct {
+	*NatsOutputConfig
+	Conn    *nats.Conn
+	Options *nats.Options
+}
+
+func (output *NatsOutput) ConfigStruct() interface{} {
+	return &NatsOutputConfig{Url: nats.DefaultURL}
+}
+
+func (output *NatsOutput) Init(config interface{}) error {
+	conf := config.(*NatsOutputConfig)
+	output.NatsOutputConfig = conf
+	options := &nats.Options{
+		Url:            conf.Url,
+		Servers:        conf.Servers,
+		NoRandomize:    conf.NoRandomize,
+		AllowReconnect: conf.AllowReconnect,
+		MaxReconnect:   conf.MaxReconnect,
+	}
+
+	if conf.ReconnectWait > 0 {
+		options.ReconnectWait = time.Duration(conf.ReconnectWait) * time.Second
+	}
+	if conf.Timeout > 0 {
+		options.Timeout = time.Duration(conf.Timeout) * time.Millisecond
+	}
+	output.Options = options
+	return nil
+}
+
+func (output *NatsOutput) Run(runner pipeline.OutputRunner,
+	helper pipeline.PluginHelper) (err error) {
+	if runner.Encoder() == nil {
+		return errors.New("Encoder required.")
+	}
+
+	output.Conn, err = output.Options.Connect()
+	if err != nil {
+		return
+	}
+	defer output.Conn.Close()
+
+	var (
+		pack     *pipeline.PipelinePack
+		outgoing []byte
+	)
+	inChan := runner.InChan()
+
+	for pack = range inChan {
+		outgoing, err = runner.Encode(pack)
+		if err != nil {
+			runner.LogError(err)
+			continue
+		}
+		err = output.Conn.Publish(output.Subject, outgoing)
+		if err != nil {
+			runner.LogError(err)
+		}
+		pack.Recycle()
+	}
+
+	return nil
+}
+
+func init() {
+	pipeline.RegisterPlugin("NatsOutput", func() interface{} {
+		return new(NatsOutput)
+	})
+}
