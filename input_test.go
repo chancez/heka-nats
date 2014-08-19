9903f05275a59d7679b6c9a17a4202a10fa85211
Chance Zibolski <chance.zibolski@gmail.com>
2014-08-18 18:15:25 -0700

    Test without decoder

--- input_test.go
+++ input_test.go
@@ -15,6 +15,7 @@ import (
 func NatsInputSpec(c gs.Context) {
 	t := new(pipeline_ts.SimpleT)
 	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
 
 	pConfig := pipeline.NewPipelineConfig(nil)
 	var wg sync.WaitGroup
@@ -57,22 +58,39 @@ func NatsInputSpec(c gs.Context) {
 		ith.MockInputRunner.EXPECT().InChan().Return(ith.PackSupply)
 		ith.MockHelper.EXPECT().PipelineConfig().Return(pConfig)
 
-		decoderName := "ScribbleDecoder"
-		config.DecoderName = decoderName
-
-		mockDecoderRunner := pipelinemock.NewMockDecoderRunner(ctrl)
-		mockDecoderRunner.EXPECT().InChan().Return(retPackChan)
-		ith.MockHelper.EXPECT().DecoderRunner(decoderName,
-			fmt.Sprintf("%s-%s", inputName, decoderName),
-		).Return(mockDecoderRunner, true)
-
 		c.Specify("that is started", func() {
-			err := input.Init(config)
-			c.Expect(err, gs.IsNil)
-
-			startInput()
-
 			c.Specify("gets messages its subscribed to", func() {
+				c.Specify("injects messages into the pipeline when not configured with a decoder", func() {
+					ith.MockInputRunner.EXPECT().Inject(ith.Pack).Do(func(p *pipeline.PipelinePack) {
+						retPackChan <- p
+					}).AnyTimes()
+
+					err := input.Init(config)
+					c.Expect(err, gs.IsNil)
+
+					c.Expect(input.DecoderName, gs.Equals, "")
+
+				})
+
+				c.Specify("injects messages into the decoder when configured", func() {
+					decoderName := "ScribbleDecoder"
+					config.DecoderName = decoderName
+
+					mockDecoderRunner := pipelinemock.NewMockDecoderRunner(ctrl)
+					mockDecoderRunner.EXPECT().InChan().Return(retPackChan)
+					ith.MockHelper.EXPECT().DecoderRunner(decoderName,
+						fmt.Sprintf("%s-%s", inputName, decoderName),
+					).Return(mockDecoderRunner, true)
+
+					err := input.Init(config)
+					c.Expect(err, gs.IsNil)
+
+					c.Expect(input.DecoderName, gs.Equals, decoderName)
+
+				})
+
+				startInput()
+
 				msg := &nats.Msg{
 					Subject: config.Subject,
 					Data:    []byte("test message"),
@@ -82,6 +100,11 @@ func NatsInputSpec(c gs.Context) {
 				pack := <-retPackChan
 
 				c.Expect(pack.Message.GetPayload(), gs.Equals, string(msg.Data))
+				val, ok := pack.Message.GetFieldValue("subject")
+				c.Assume(ok, gs.IsTrue)
+				subj, ok := val.(string)
+				c.Assume(ok, gs.IsTrue)
+				c.Expect(subj, gs.Equals, config.Subject)
 			})
 
 			input.Stop()
